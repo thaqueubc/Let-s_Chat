be889e8329a3802d7bc6a2e02374a1e2e16f0794
thaqueubc <reya.ruet@gmail.com>
2020-08-18 10:15:58 -0700

    Connect to Mongodb & Socket.io

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const mongo = require('mongodb').MongoClient;
-const client = require('socket.io').listen(4000);
+const client = require('socket.io').listen(4000).sockets;
 
 // Connect to Mongo
-mongo.connect('mongodb://127.0.0.1/letsChat', function(err, db){
-    if(err){
-        throw err;
-    }
+const url = 'mongodb://127.0.0.1/letsChat';
 
-    console.log('MongoDB is connected...');
-})
+mongo.connect(
+    url,
+     { useNewUrlParser: true, useUnifiedTopology: true },
+     //callback function
+      (err, db) => { 
+        if (err) {throw err;}
+    
+        console.log('MongoDB is connected...');
+
+        // Connect to socket.io
+        client.on('connection', function(){
+            let chat = db.collection('chats');
+
+            // Create function to send status
+            sendStatus = function(s){
+                socket.emit('status',s);
+            }
+        })
+
+      }
+    );
