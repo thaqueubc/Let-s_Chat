24518e738ebb089fd6a79709e535732259503302
thaqueubc <reya.ruet@gmail.com>
2020-08-18 10:21:58 -0700

    Create collection & get chat messages

--- server.js
+++ server.js
@@ -15,12 +15,23 @@ mongo.connect(
 
         // Connect to socket.io
         client.on('connection', function(){
+            //Create a collection to store chats
             let chat = db.collection('chats');
 
             // Create function to send status
             sendStatus = function(s){
                 socket.emit('status',s);
             }
+
+            //Get Chats from mongo Collection
+            chat.find().limit(100).sort({_id:1}).toArray(function(err,res){
+                if (err) {throw err;}
+
+                //Emit the ,emit the messages
+                socket.emit('output', res);
+            });
+
+            
         })
 
       }
