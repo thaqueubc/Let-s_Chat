f23ed6775c0d95cd55520d2e4166f72efe44285c
thaqueubc <reya.ruet@gmail.com>
2020-08-24 14:29:50 -0700

    Router issue is fixed

--- client/src/app.js
+++ client/src/app.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 
-const App = () =>(
-   <Router>
-        <Route path="/" component={Join} />
-        <Route path="/chat" component={Chat} />
-   </Router>
-);
+const App = () =>{
+    return (
+        <Router>
+             <Route path="/" exact component={Join} />  
+             <Route path="/chat" component={Chat} />
+        </Router>
+     );
+}
 
 export default App;
--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Chat = () =>{
     return (
         <h1>Chat</h1>
-    )
+    );
 }
 
 export default Chat;
